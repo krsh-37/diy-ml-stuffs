d5cb86aa132f26c6e85ee94aa350c056b1b1ce9a
Krishnaa Kannan <36710708+krsh-37@users.noreply.github.com>
2025-08-23 13:41:44 +0530

    base dumb vit

---
+++ vision transformer/vit_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+class VisionTransformer(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+    
+    def forward(self, ):
+        pass
+
+if __name__ == "__main__":
+    VisionTransformer()
